c128bf82e6e61c2745b7a31abcb4ef44943b9dbd
Sixfr <sixfr@sixfr.com>
2015-12-08 16:36:38 +0100

    dfsdfgs

--- test_github_project/src/com/adaming/bo/Employe.java
+++ test_github_project/src/com/adaming/bo/Employe.java
@@ -6,15 +6,13 @@ public class Employe {
 	private String nom;
 	
 	
-<<<<<<< HEAD
+
 	public Employe(int id, String nom) {
 		super();
 		this.id = id;
 		this.nom = nom;
 	}
 	
-	
-=======
->>>>>>> branch 'master' of https://github.com/Sixfr/coursAdaming.git
+
 
 }
